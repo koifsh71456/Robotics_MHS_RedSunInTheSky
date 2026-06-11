51032ae5c2e2681fab28da9984615169ae917938
koifsh71456 <koifsh71456@gmail.com>
2026-06-11 16:04:51 +1000

    Adding colour checks to test1.py and adding rescueLineScript

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-# this is an example
-# this is another example
+# this is an example to test if the bot can detect the black colour
+
+from ev3dev2.sensor.lego import ColorSensor, INPUT_1
+from time import sleep
+
+cl = ColorSensor(INPUT_1)
+
+cl.mode = ColorSensor.MODE_COL_COLOR
+
+if cl.color == ColorSensor.COLOR_BLACK:
+    print("Black detected")
+elif cl.color == ColorSensor.COLOR_GREEN:
+    print("Green detected")
+else:
+    print("Other color detected")
