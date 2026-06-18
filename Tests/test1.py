08eff743a86ce7ec2bf43d6a7a3179cec34c010b
koifsh71456 <koifsh71456@gmail.com>
2026-06-18 16:17:08 +1000

    followLine() initialized, returning direction to go
    Co-authored-by: blobfsh41 <blobfsh41@users.noreply.github.com>
    Co-authored-by: TommyTao679 <TommyTao679@users.noreply.github.com>

--- Tests/test1.py
+++ Tests/test1.py
@@ -9,7 +9,10 @@ cl_1 = ColorSensor(INPUT_1)
 # cl_3 = ColorSensor(INPUT_3)
 cl_4 = ColorSensor(INPUT_4)
 
+# INPUT_1 is the right colour sensor, INPUT_4 is left
+
 cl_1.mode = ColorSensor.MODE_COL_COLOR
+cl_4.mode = ColorSensor.MODE_COL_COLOR
 
 if cl_1.color == ColorSensor.COLOR_BLACK:
     print("Black detected")
@@ -17,3 +20,22 @@ elif cl_1.color == ColorSensor.COLOR_GREEN:
     print("Green detected")
 else:
     print("Other color detected")
+
+def followLine():
+    # the direction variable gets returned by this function which will determine which
+    # way the robot goes based on the colour sensors.
+    direction = ""
+    if cl_1.color == ColorSensor.COLOR_BLACK:
+        direction = "right"
+    if cl_4.color == ColorSensor.COLOR_BLACK:
+        direction = "left"
+    if cl_1.color == ColorSensor.COLOR_BLACK and cl_4.color == ColorSensor.COLOR_BLACK:
+        direction = "intersection"
+        # intersections will have a green square to guide to the correct direction
+        # the robot must move so one of its colour sensors can detect the green
+        # one way this could work is moving the robot back a little until it detects green
+        
+    return direction
+        
+
+
