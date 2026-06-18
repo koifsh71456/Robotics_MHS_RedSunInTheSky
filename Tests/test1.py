33ef0c30d064a90eddfc4d1612b43aac719741e8
TommyTao679 <tommytao231121@gmail.com>
2026-06-18 15:49:28 +1000

    Setting input sources for test1.py

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # this is an example to test if the bot can detect the black colour
 
-from ev3dev2.sensor.lego import ColorSensor, INPUT_1
+from ev3dev2.sensor.lego import ColorSensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from time import sleep
 
-cl = ColorSensor(INPUT_1)
+cl_1 = ColorSensor(INPUT_1)
+# cl_2 = ColorSensor(INPUT_2)
+# cl_3 = ColorSensor(INPUT_3)
+cl_4 = ColorSensor(INPUT_4)
 
-cl.mode = ColorSensor.MODE_COL_COLOR
+cl_1.mode = ColorSensor.MODE_COL_COLOR
 
-if cl.color == ColorSensor.COLOR_BLACK:
+if cl_1.color == ColorSensor.COLOR_BLACK:
     print("Black detected")
-elif cl.color == ColorSensor.COLOR_GREEN:
+elif cl_1.color == ColorSensor.COLOR_GREEN:
     print("Green detected")
 else:
     print("Other color detected")
