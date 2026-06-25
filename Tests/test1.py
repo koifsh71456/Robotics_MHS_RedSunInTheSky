18af7eecd1cd40b4b1ad43a6a90a621a48a4620e
koifsh71456 <koifsh71456@gmail.com>
2026-06-25 16:10:09 +1000

    Finalize display

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # this is an example to test if the bot can detect the black colour
 
-from ev3dev2.sensor.lego import ColorSensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor import Sensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
 from ev3dev2.display import Display
 from time import sleep
@@ -18,15 +19,35 @@ motors = MoveTank(OUTPUT_B, OUTPUT_C)
 cl_1.mode = ColorSensor.MODE_COL_COLOR
 cl_4.mode = ColorSensor.MODE_COL_COLOR
 
+display.text_pixels("Hello!", x=10, y=10, clear_screen=True)
+display.update()
+
 while True:
     if cl_1.color == ColorSensor.COLOR_BLACK:
-        display.text_pixels("Black Detected", x=10, y=10, clear_screen=True)
+        display.clear()
+        display.text_pixels("Black Detected (Right)", x=10, y=10, clear_screen=True)
         display.update()
     elif cl_1.color == ColorSensor.COLOR_GREEN:
-        display.text_pixels("Green Detected", x=10, y=30, clear_screen=False)
+        display.clear()
+        display.text_pixels("Green Detected (Right)", x=10, y=30, clear_screen=False)
         display.update()
     else:
-        display.text_pixels("Nothing Detected", x=10, y=30, clear_screen=False)
+
+        display.text_pixels("Nothing Detected (Right)", x=10, y=30, clear_screen=False)
+        display.update()
+
+    if cl_4.color == ColorSensor.COLOR_BLACK:
+        display.clear()
+        display.text_pixels("Black Detected (Left)", x=10, y=50, clear_screen=True)
+        display.update()
+
+    elif cl_4.color == ColorSensor.COLOR_GREEN:
+        display.clear()
+        display.text_pixels("Green Detected (Left)", x=10, y=70, clear_screen=False)
+        display.update()
+
+    else:
+        display.text_pixels("Nothing Detected (Left)", x=10, y=70, clear_screen=False)
         display.update()
 
 
@@ -40,11 +61,13 @@ def followLine():
         direction = "left"
     if cl_1.color != ColorSensor.COLOR_BLACK and cl_4.color != ColorSensor.COLOR_BLACK:
         direction = "forward"
-    if cl_1.color == ColorSensor.COLOR_BLACK and cl_4.color == ColorSensor.COLOR_BLACK:
-        direction = "intersection"
-        # intersections will have a green square to guide to the correct direction
-        # the robot must move so one of its colour sensors can detect the green
-        # one way this could work is moving the robot back a little until it detects green
+    # intersections will have a green square to guide to the correct direction
+    if cl_1.color == ColorSensor.COLOR_GREEN:
+        direction = "intersectionRight"
+    if cl_4.color == ColorSensor.COLOR_GREEN:
+        direction = "intersectionLeft"
+
+
         
     return direction
         
