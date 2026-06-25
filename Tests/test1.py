e48e5ff7e47ca1cb574d8c4e1f4936fb92c460b6
koifsh71456 <koifsh71456@gmail.com>
2026-06-25 15:58:50 +1000

    Adding display for debug purposes
    Co-authored-by: TommyTao679 <TommyTao679@users.noreply.github.com>

--- Tests/test1.py
+++ Tests/test1.py
@@ -2,24 +2,34 @@
 # this is an example to test if the bot can detect the black colour
 
 from ev3dev2.sensor.lego import ColorSensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
+from ev3dev2.display import Display
 from time import sleep
 
+# Initialise the display, colour sensors and motors
+display = Display()
 cl_1 = ColorSensor(INPUT_1)
 # cl_2 = ColorSensor(INPUT_2)
 # cl_3 = ColorSensor(INPUT_3)
 cl_4 = ColorSensor(INPUT_4)
+motors = MoveTank(OUTPUT_B, OUTPUT_C)
 
 # INPUT_1 is the right colour sensor, INPUT_4 is left
-
 cl_1.mode = ColorSensor.MODE_COL_COLOR
 cl_4.mode = ColorSensor.MODE_COL_COLOR
 
-if cl_1.color == ColorSensor.COLOR_BLACK:
-    print("Black detected")
-elif cl_1.color == ColorSensor.COLOR_GREEN:
-    print("Green detected")
-else:
-    print("Other color detected")
+while True:
+    if cl_1.color == ColorSensor.COLOR_BLACK:
+        print("Black detected")
+        display.text_pixels("Black Detected", x=10, y=10, clear_screen=True)
+        display.update()
+    elif cl_1.color == ColorSensor.COLOR_GREEN:
+        print("Green detected")
+        display.text_pixels("Green Detected", x=10, y=30, clear_screen=False)
+        display.update()
+    else:
+        print("Other color detected")
+
 
 def followLine():
     # the direction variable gets returned by this function which will determine which
