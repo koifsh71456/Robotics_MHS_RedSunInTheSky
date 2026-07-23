aeb0a3a003e3a26918b608b9ef845441b62519ff
TommyTao679 <tommytao231121@gmail.com>
2026-07-23 20:19:17 +1000

    Commented out colour detection test code, and added the main robot movement loop

--- Tests/test1.py
+++ Tests/test1.py
@@ -22,40 +22,10 @@ cl_4.mode = ColorSensor.MODE_COL_COLOR
 display.text_pixels("Hello!", x=10, y=10, clear_screen=True)
 display.update()
 
-while True:
-    display.clear()
-    # if cl_1.color == ColorSensor.COLOR_BLACK:
-    #     display.text_pixels("Black Detected (Right)", x=10, y=10, clear_screen=False)
-
-    # elif cl_1.color == ColorSensor.COLOR_RED:
-    #     display.text_pixels("Red Detected (Right)", x=10, y=30, clear_screen=False)
-
-    # elif cl_1.color == ColorSensor.COLOR_GREEN:
-    #     display.text_pixels("Green Detected (Right)", x=10, y=30, clear_screen=False)
-
-    # elif cl_1.color == ColorSensor.COLOR_RED:
-    #     display.text_pixels("Red Detected (Right)", x=10, y=30, clear_screen=False)
-
-    # else:
-    #     display.text_pixels("Nothing Detected (Right)", x=10, y=30, clear_screen=False)
-
-    # if cl_4.color == ColorSensor.COLOR_BLACK:
-    #     display.text_pixels("Black Detected (Left)", x=10, y=50, clear_screen=False)
-
-    # elif cl_4.color == ColorSensor.COLOR_RED:
-    #     display.text_pixels("Red Detected (Left)", x=10, y=70, clear_screen=False)
-
-    # elif cl_4.color == ColorSensor.COLOR_GREEN:
-    #     display.text_pixels("Green Detected (Left)", x=10, y=70, clear_screen=False)
-
-    # else:
-    #     display.text_pixels("Nothing Detected (Left)", x=10, y=70, clear_screen=False)
-
-    # display.text_pixels("Right: {cl_1.color} Left: {cl_4.color}"
-    # , x=10, y=30, clear_screen=False)
-    display.text_pixels("Right:" + str(cl_1.color_name) + " Left:" + str(cl_4.color_name), x=10, y=30, clear_screen=False)
-
-    display.update()
+# while True:
+#     display.clear()
+#     display.text_pixels("Right:" + str(cl_1.color_name) + " Left:" + str(cl_4.color_name), x=10, y=30, clear_screen=False)
+#     display.update()
 
 
 def followLine():
@@ -76,3 +46,18 @@ def followLine():
         direction = "intersectionLeft"
         
     return direction
+
+while True:
+    direction = followLine()
+    if direction == "right":
+        motors.on(left_speed=0, right_speed=20)
+    elif direction == "left":
+        motors.on(left_speed=20, right_speed=0)
+    elif direction == "forward":
+        motors.on(left_speed=20, right_speed=20)
+    elif direction == "intersectionRight":
+        motors.on(left_speed=0, right_speed=20)
+    elif direction == "intersectionLeft":
+        motors.on(left_speed=20, right_speed=0)
+    else:
+        motors.off()
